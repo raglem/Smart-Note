491ad1c6c126bb96ecca6e7a556aa807bf2ae7dc
Theoden Melgar <theodenmelgar@gmail.com>
2025-08-10 23:01:21 -0700

    added Join component to search for and join classes

--- frontend/src/components/Class/LeaveClass.tsx
+++ frontend/src/components/Class/LeaveClass.tsx
@@ -1,16 +1,18 @@
+"use client"
+
 import { ClassContext } from "@/app/context/ClassContext"
 import api from "@/utils/api"
 import router from "next/router"
 import { useContext } from "react"
 
-export default function DeleteClass({ close} : {
+export default function LeaveClass({ close} : {
     close: () => void,
 }){
     const { classFields } = useContext(ClassContext)
     const { id, name } = classFields
-    const handleDelete = async () => {
+    const handleLeave = async () => {
         try{
-            const res = await api.delete(`/classes/${id}/`)
+            const res = await api.delete(`/classes/leave/${id}/`)
             router.push('/classes')
         }
         catch(err){
@@ -19,18 +21,18 @@ export default function DeleteClass({ close} : {
     }
     return (
         <div className="overlay">
-            <div className="card flex flex-col py-4 gap-y-2 min-w-[300px] w-[50vw] max-w-[768px] bg-secondary">
+            <div className="card flex flex-col py-4 gap-y-2 min-w-[300px] w-[50vw] max-w-[768px] bg-white border-1 border-primary">
                 <header className="flex flex-row items-center px-2 text-3xl border-b-1 border-b-primary">
-                    Delete Class
+                    Leave Class
                 </header>
-                <p className="px-2">
-                    <span className="text-2xl">Are you sure you want to delete {name}?</span>
+                <p className="p-2">
+                    <span className="text-2xl">Are you sure you want to leave {name}?</span>
                     <br/>
                     <i className="text-md">This action cannot be undone</i>
                 </p>
                 <div className="form-btn-toolbar px-2">
-                    <button onClick={handleDelete} className="form-btn bg-primary text-white">Delete</button>
-                    <button onClick={close} className="form-btn bg-secondary text-primary">Cancel</button>
+                    <button onClick={handleLeave} className="form-btn bg-red-500 text-white">Leave</button>
+                    <button onClick={close} className="form-btn bg-white text-black">Cancel</button>
                 </div>
             </div>
         </div>
