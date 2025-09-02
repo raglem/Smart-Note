934fab37330d549da6e60b44079688ad73017198
Theoden Melgar <theodenmelgar@gmail.com>
2025-09-01 18:12:18 -0700

    polished home page to showcase website features and have a button logging in to the test_user

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,8 +6,9 @@ import { usePathname } from "next/navigation";
 import Link from "next/link"
 import Logout from "@/components/Logout";
 import { FaSearch } from "react-icons/fa";
-import { MdAccountCircle } from "react-icons/md";
+import { MdAccountCircle, MdQuiz } from "react-icons/md";
 import { UserContext } from "@/app/context/UserContext";
+import { CgNotes } from "react-icons/cg";
 
 export default function Navbar(){
     const { userId, checkUser } = useContext(UserContext)
@@ -21,16 +22,13 @@ export default function Navbar(){
 
     if(!userId){
       return (
-        <nav className="flex flex-row justify-between items-center h-[60px] w-[100vw] p-2 bg-primary text-2xl text-white">
-          <div className="flex flex-row items-end gap-x-10">
-            <span className="text-3xl">
-              SmartNote
-            </span>
-            <ol className="flex flex-row gap-x-5 items-center">
-              <li>
-                <Link href="/">Home</Link>
-              </li>
-            </ol>
+        <nav className="flex flex-row justify-between items-center h-[60px] w-[100vw] py-2 px-4 bg-primary text-2xl text-white">
+          <div className="flex flex-row items-center gap-x-10">
+            <Link href="/">
+              <span className="text-3xl cursor-pointer hover:opacity-80">
+                SmartNote
+              </span>
+            </Link>
           </div>
           <div className="flex flex-row items-center gap-x-2">
             <Link href="/login"> 
@@ -42,24 +40,32 @@ export default function Navbar(){
     }
 
     return (
-      <nav className="flex flex-row justify-between items-center h-[60px] w-[100vw] p-2 bg-primary text-2xl text-white">
+      <nav className="flex flex-row justify-between items-center h-[60px] w-[100vw] py-2 px-4 bg-primary text-2xl text-white">
         <div className="flex flex-row items-end gap-x-10">
-          <span className="text-3xl">
-            SmartNote
-          </span>
+            <Link href="/">
+              <span className="text-3xl cursor-pointer hover:opacity-80">
+                SmartNote
+              </span>
+            </Link>
           <ol className="flex flex-row gap-x-5 items-center">
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/classes">Classes</Link>
-            </li>
-            <li>
-              <Link href="/quizzes">Quizzes</Link>
-            </li>
-            <li>
-              <Link href="/study-groups">Study Groups</Link>
-            </li>
+            <Link href="/classes">
+              <li className="flex flex-row gap-x-1 items-center"> 
+                <CgNotes className="icon-responsive" />
+                Classes
+              </li>
+            </Link>
+            <Link href="/quizzes">
+              <li className="flex flex-row gap-x-1 items-center"> 
+              <MdQuiz className="icon-responsive" />
+                Quizzes
+              </li>
+            </Link>
+            <Link href="/study-groups">
+              <li className="flex flex-row gap-x-1 items-center"> 
+                <CgNotes className="icon-responsive" />
+                Study Groups
+              </li>
+            </Link>
           </ol>
         </div>
         <div className="flex flex-row gap-x-2">
